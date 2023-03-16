ff42eb38356642ae7fd826368c35b52e79188aaa
PavelFilimonov <chief.filimonov@yandex.ru>
2023-03-16 22:21:55 +0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Footer.css';
+
+import TasksFilter from '../TasksFilter/';
+
+export default class Footer extends Component {
+  render() {
+    const { backlogTasks, clearCompleted, changeFilter, filter } = this.props;
+    return (
+      <footer className="footer">
+        <span className="todo-count">
+          {backlogTasks} {backlogTasks > 1 ? 'items' : 'item'} left
+        </span>
+        <TasksFilter changeFilter={changeFilter} filter={filter} />
+        <button type="button" className="clear-completed" onClick={clearCompleted}>
+          Clear completed
+        </button>
+      </footer>
+    );
+  }
+}
+
+Footer.propTypes = {
+  backlogTasks: PropTypes.number,
+  clearCompleted: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  backlogTasks: 0,
+  filter: 'All',
+};
